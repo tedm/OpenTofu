ce54884c6c4091085c71de50efef604142a5474e
tedm <matsumura@gmail.com>
2026-07-08 21:25:48 -0700

    Add AWS Load Balancer Controller via Terraform IRSA + Helm; remove manual ingressclass/namespace files

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -2,6 +2,30 @@ provider "aws" {
   region = var.region
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
 # Filter out local zones, which are not currently supported 
 # with managed node groups
 data "aws_availability_zones" "available" {
@@ -50,7 +74,7 @@ module "eks" {
   version = "20.8.5"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.33"
+  cluster_version = "1.36"
 
   cluster_endpoint_public_access           = true
   enable_cluster_creator_admin_permissions = true
@@ -75,7 +99,7 @@ module "eks" {
       instance_types = ["t3.small"] # lesser instance ebs-csi driver will timeout
 
       min_size     = 1
-      max_size     = 3
+      max_size     = 2
       desired_size = 1
     }
 /*
@@ -85,7 +109,7 @@ module "eks" {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 3
+      max_size     = 2
       desired_size = 1
     }
 */
@@ -107,4 +131,71 @@ module "irsa-ebs-csi" {
   provider_url                  = module.eks.oidc_provider
   role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
   oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+# ---------------------------------------------------------------------------
+# AWS Load Balancer Controller
+# ---------------------------------------------------------------------------
+
+# Policy JSON pulled from:
+# https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.14.1/docs/install/iam_policy.json
+# Place iam_policy.json in the same directory as this file.
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+  name   = "AWSLoadBalancerControllerIAMPolicy-${module.eks.cluster_name}"
+  policy = file("${path.module}/iam_policy.json")
+}
+
+module "irsa-lb-controller" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSLoadBalancerControllerRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [aws_iam_policy.aws_load_balancer_controller.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+}
+
+resource "kubernetes_service_account" "aws_load_balancer_controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+      "app.kubernetes.io/component" = "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn" = module.irsa-lb-controller.iam_role_arn
+    }
+  }
+
+  depends_on = [module.eks]
+}
+
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.13.4" # chart version matching controller image v3.4.1 - verify current version before apply
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.aws_load_balancer_controller.metadata[0].name
+  }
+
+  depends_on = [
+    kubernetes_service_account.aws_load_balancer_controller,
+    module.eks
+  ]
 }
--- aws/eks/terraform.tf
+++ aws/eks/terraform.tf
@@ -1,23 +1,26 @@
-
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.47.0"
     }
-
     random = {
       source  = "hashicorp/random"
       version = "~> 3.6.1"
     }
-
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0.5"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.33"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.15"
+    }
   }
-
   backend "s3" {
     bucket         = "ted-terraform-tfstate"
     key            = "state/terraform.tfstate"
@@ -25,7 +28,5 @@ terraform {
     encrypt        = true
     dynamodb_table = "ted-tf-lockid"
   }
-
   required_version = "~> 1.3"
 }
-
